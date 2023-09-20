126a427aae91c91511d21124817a283d30c9d70b
LTH3ar <huylt.bi12-195@st.usth.edu.vn>
2023-09-20 15:37:53 +0700

    test update

--- demo_app/app/build.gradle.kts
+++ demo_app/app/build.gradle.kts
@@ -16,6 +16,7 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -29,6 +30,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -36,9 +40,9 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.cardview:cardview:1.0.0")
-    implementation ("me.relex:circleindicator:2.1.6")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("com.ramotion.paperonboarding:paper-onboarding:1.1.3")
+    implementation("com.github.AppIntro:AppIntro:6.3.1")
 }
